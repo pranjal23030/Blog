86ce549030cb2095f0ad321e3c0f8532af92e4cc
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-08-09 21:27:46 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')            // express from mode_values
+const app = express()                         // express methods via app variable
+
+app.get("/",(req,res)=>{                      // Request - Response cycle browser==>requests
+    // console.log(req)
+    // res.send("Hello world")
+    res.json({
+        message: "Hello pranjal"
+    })
+})
+
+app.get("/about",(req,res)=>{
+    res.json({
+        message: "This is the about page"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("NodeJs project has started")
+})
