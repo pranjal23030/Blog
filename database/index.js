1ca6edd207051b9a026be27a543a73f3fbc8a4f3
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-08-14 23:22:19 +0545

    priv-env

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose') // CJS {Common JS}
 
 async function connectToDatabase() {
-    await mongoose.connect('mongodb+srv://pranjalKhatiwada:pranjalmern@cluster0.dgwm7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log("Database connected successfully.")
 }
 
