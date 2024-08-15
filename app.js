9287fb6dfd4cb1c66ea60a9b562f47d9ed889ddd
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-08-15 21:15:19 +0545

    post blog-api

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ require('dotenv').config()
 const express = require('express')                          // express from mode_values
 const connectToDatabase = require('./database/')            // Auto import index.js , need route if another name
 const app = express()                                       // express methods via app variable
+app.use(express.json())                                     // Understanding json format in express
 
 connectToDatabase()
 
-app.get("/",(req,res)=>{                      // Request - Response cycle, browser==>requests
+app.get("/",(req,res)=>{                                    // Request - Response cycle, browser==>requests
     // console.log(req)
     // res.send("Hello world")
-    res.status(200).json({                     // Success status code
+    res.status(200).json({                                  // Success status code
         message: "Hello world"
     })
 })
@@ -17,6 +18,13 @@ app.get("/",(req,res)=>{                      // Request - Response cycle, brows
 app.get("/about",(req,res)=>{
     res.json({
         message: "This is the about page"
+    })
+})
+
+app.post("/blog",(req, res)=>{
+    console.log(req.body)                                   // Accepting the data posted
+    res.status(200).json({
+        message: "Blog api hit successfully"
     })
 })
 
