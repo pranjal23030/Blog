16ddf9eff2c2cf8fb59f252dd5b0f3d35de05bd2
pranjal23030 <pranjal.khatiwoda@gmail.com>
2024-08-21 09:28:05 +0545

    blog find and create

--- app.js
+++ app.js
@@ -27,28 +27,39 @@ app.get("/about",(req,res)=>{
 
 app.post("/blog", upload.single('image') ,async (req, res)=>{
     // console.log(req.body)                                                            // Accepting the data posted, {objects} and sending to database
-    // const title = req.body.title
-    // const subtitle = req.body.subtitle
-    // const description = req.body.description
-    // const image = req.body.image
 
-    // if(!title || !subtitle || !description || !image) {
-    //     return res.status(400).json({                                                   // Doesn't execute add data code
-    //         message: "Please provide title, subtitle, description, image"
-    //     })
-    // }
+    const title = req.body.title
+    const subtitle = req.body.subtitle
+    const description = req.body.description
+    const filename = req.file.filename
 
-    // await Blog.create({
-    //     title : title,                                                                  // Column name : data from frontend 
-    //     subtitle : subtitle,
-    //     description : description,
-    //     image : image
-    // })
+    if(!title || !subtitle || !description) {
+        return res.status(400).json({                                                   // Doesn't execute add data code
+            message: "Please provide title, subtitle, description."
+        })
+    }
+
+    await Blog.create({
+        title : title,                                                                  // Column name : data from frontend 
+        subtitle : subtitle,
+        description : description,
+        image: filename
+    })
 
     res.status(200).json({
         message: "Blog api hit successfully"
     })
 })
+
+app.get("/blog",async(req,res)=>{            // Kinda like CRUD Read API
+    const blogs = await Blog.find()         // returns array
+    res.status(200).json({
+        message: "Blogs fetched successfully",
+        data: blogs
+    })
+})
+
+app.use(express.static('./storage'))            // Image read permission
 
 app.listen(process.env.PORT,()=>{
     console.log("NodeJs project has started")
